ae3891885253ce3703fa1be8ff0896d77c510e3f
Joe Bruzek <jbruzek@vt.edu>
2015-04-28 21:37:37 -0400

    restyled index.html

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
 Parse.initialize("csMOVllLLOUIenFmTG1oH6ayhdinQnWpYr0VwGIx", "soFGuQMj57zBvU6PFHHJQxL05kcQWEV90QWmLJId");
 
+var mapstyle = [
+  {
+    "featureType": "poi",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "weight": 7.5 },
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "landscape.natural",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "landscape",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "color": "#FFC107" }
+    ]
+  },{
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "elementType": "labels.icon",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "transit",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "water",
+    "stylers": [
+      { "color": "#ADD8E6" }
+    ]
+  }
+];
 var locations = [];
 
 var parseloc = Parse.Object.extend("locations");
@@ -22,13 +68,19 @@ query.find({
 });
 
 function initialize() {
-	var mapCanvas = document.getElementById('map_canvas');
+	var styledMap = new google.maps.StyledMapType(mapstyle, {name: "Styled Map"});
+	var mapCanvas = document.getElementById('map-canvas');
 	var mapOptions = {
-	  center: new google.maps.LatLng(37.2303918, -80.4218075),
-	  zoom: 13,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
+		center: new google.maps.LatLng(37.233522, -80.464406),
+		zoom: 13,
+		disableDefaultUI: true,
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+		}
 	}
 	var map = new google.maps.Map(mapCanvas, mapOptions);
+	map.mapTypes.set('map_style', styledMap);
+  	map.setMapTypeId('map_style');
 
 	for (var i = 0; i < locations.length; i++) {
 		var marker = new google.maps.Marker({
