a62644684af10e4ed2ff5b3dc5ab5c8c8fc1d492
Joe Bruzek <jbruzek@vt.edu>
2015-11-19 12:37:01 -0500

    Added new design files to redesign branch

--- main.js
+++ main.js
@@ -1,92 +1,17 @@
-Parse.initialize("csMOVllLLOUIenFmTG1oH6ayhdinQnWpYr0VwGIx", "soFGuQMj57zBvU6PFHHJQxL05kcQWEV90QWmLJId");
+window.onload = function() {
+	//loaded
+}
 
-var mapstyle = [
-  {
-    "featureType": "poi",
-    "elementType": "geometry.fill",
-    "stylers": [
-      { "visibility": "on" },
-      { "weight": 7.5 },
-      { "color": "#ffffff" }
-    ]
-  },{
-    "featureType": "landscape.natural",
-    "stylers": [
-      { "color": "#ffffff" }
-    ]
-  },{
-    "featureType": "landscape",
-    "stylers": [
-      { "color": "#ffffff" }
-    ]
-  },{
-    "featureType": "road",
-    "stylers": [
-      { "color": "#FFC107" }
-    ]
-  },{
-    "elementType": "labels.text.fill",
-    "stylers": [
-      { "visibility": "off" }
-    ]
-  },{
-    "elementType": "labels.icon",
-    "stylers": [
-      { "visibility": "off" }
-    ]
-  },{
-    "featureType": "transit",
-    "stylers": [
-      { "color": "#ffffff" }
-    ]
-  },{
-    "featureType": "water",
-    "stylers": [
-      { "color": "#ADD8E6" }
-    ]
-  }
-];
-var locations = [];
+document.onscroll = function() {
+	var scroll = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+	var headerSize = document.getElementById("header").clientHeight - document.getElementById("nav").clientHeight;
 
-var parseloc = Parse.Object.extend("locations");
-var query = new Parse.Query(parseloc);
-query.find({
-  success: function(results) {
-    for (var i = 0; i < results.length; i++) {
-    	locations[i] = {
-    		latitude: results[i].get("latitude"),
-    		longitude: results[i].get("longitude"),
-    		name: results[i].get("name")
-    	}
-    }
-
-    initialize();
-  },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
-
-function initialize() {
-	var styledMap = new google.maps.StyledMapType(mapstyle, {name: "Styled Map"});
-	var mapCanvas = document.getElementById('map-canvas');
-	var mapOptions = {
-		center: new google.maps.LatLng(37.233522, -80.464406),
-		zoom: 13,
-		disableDefaultUI: true,
-		mapTypeControlOptions: {
-			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-		}
-	}
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-	map.mapTypes.set('map_style', styledMap);
-  	map.setMapTypeId('map_style');
-
-	for (var i = 0; i < locations.length; i++) {
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
-			map: map,
-			title: locations[i].name
-		});
+	if (scroll > headerSize) {
+		var nav = document.getElementById("nav");
+		nav.style.backgroundColor = "#009688";
+	} else {
+		var nav = document.getElementById("nav");
+		var percent = scroll/headerSize;
+		nav.style.backgroundColor = "rgba(0,150,136," + percent + ")";
 	}
 }
